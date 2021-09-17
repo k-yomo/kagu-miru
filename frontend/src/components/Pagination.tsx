c76472096b9a8e49fc2d29cc72457f96a60c54fb
k-yomo <kanji.yy@gmail.com>
2021-09-17 23:33:40 +0900

    Fix dark mode styling of pagination

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -26,16 +26,18 @@ export default function Pagination({ page, totalPage, onClickPage }: Props) {
   ];
 
   return (
-    <nav className="w-full border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
+    <nav className="w-full border-t border-gray-200 dark:border-gray-400 px-4 flex items-center justify-between sm:px-0">
       <div className="-mt-px w-0 flex-1 flex">
         <button
           disabled={page === 1}
           onClick={() => onClickPage(page - 1)}
-          className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 disabled:text-gray-300 hover:text-gray-700 disabled:hover:text-gray-300 disabled:hover:border-transparent"
+          className="border-t-2 disabled:border-none border-transparent pt-4 pr-1 inline-flex items-center text-sm ftext-gray-500 dark:text-gray-300 disabled:text-gray-300 dark:disabled:text-gray-500 hover:text-gray-700 dark:hover:text-gray-100 disabled:hover:text-gray-300 dark:disabled:hover:text-gray-500 disabled:hover:border-transparent"
         >
           <ArrowNarrowLeftIcon
             className={`mr-3 h-5 w-5 ${
-              page === 1 ? 'text-gray-300' : 'text-gray-400'
+              page === 1
+                ? 'text-gray-300 dark:text-gray-700'
+                : 'text-gray-500 dark:text-gray-300'
             }`}
             aria-hidden="true"
           />
@@ -48,9 +50,9 @@ export default function Pagination({ page, totalPage, onClickPage }: Props) {
             key={p}
             className={`${
               p === page
-                ? 'border-black text-black font-bold'
-                : 'border-transparent text-gray-400 hover:text-gray-700 hover:border-gray-300'
-            } cursor-pointer border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium`}
+                ? 'border-black dark:border-white text-black dark:text-text-primary-dark font-bold'
+                : 'border-transparent text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 hover:border-gray-300'
+            } cursor-pointer border-t-2 pt-4 px-4 inline-flex items-center text-sm`}
             onClick={() => onClickPage(p)}
           >
             {p}
@@ -61,12 +63,14 @@ export default function Pagination({ page, totalPage, onClickPage }: Props) {
         <button
           disabled={page === totalPage}
           onClick={() => onClickPage(page + 1)}
-          className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 disabled:text-gray-300 hover:text-gray-700 disabled:hover:text-gray-300 disabled:hover:border-transparent"
+          className="border-t-2 disabled:border-none border-transparent pt-4 pr-1 inline-flex items-center text-sm text-gray-500 dark:text-gray-300 disabled:text-gray-300 dark:disabled:text-gray-500 hover:text-gray-700 dark:hover:text-gray-100 disabled:hover:text-gray-300 dark:disabled:hover:text-gray-500 disabled:hover:border-transparent"
         >
           次へ
           <ArrowNarrowRightIcon
             className={`ml-3 h-5 w-5 ${
-              page === 1 ? 'text-gray-300' : 'text-gray-400'
+              page === totalPage
+                ? 'text-gray-500 dark:text-gray-700'
+                : 'text-gray-500 dark:text-gray-300'
             }`}
             aria-hidden="true"
           />
