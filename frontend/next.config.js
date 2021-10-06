77e8b24d47aae07ed7f300e06337aac65b1a3712
k-yomo <kanji.yy@gmail.com>
2021-10-06 20:14:58 +0900

    Format frontend code

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,54 +1,52 @@
 /** @type {import('next').NextConfig} */
 
 const securityHeaders = [
-    {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'on'
-    },
-    {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload'
-    },
-    {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block'
-    },
-    {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN'
-    },
-    {
-        key: 'Permissions-Policy',
-        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
-    },
-    {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff'
-    },
-    {
-        key: 'Referrer-Policy',
-        value: 'origin-when-cross-origin'
-    },
-    {
-        key: 'Content-Security-Policy',
-        value: "default-src 'self'; img-src *; script-src 'self' www.googletagmanager.com"
-    }
-]
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; img-src *; script-src 'self' www.googletagmanager.com",
+  },
+];
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-        'thumbnail.image.rakuten.co.jp',
-        'via.placeholder.com'
-    ],
+    domains: ['thumbnail.image.rakuten.co.jp', 'via.placeholder.com'],
   },
-async headers() {
+  async headers() {
     return [
-        {
-            source: '/(.*)',
-            headers: securityHeaders,
-        },
-    ]
-},
-}
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,31 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.{ts,tsx}'],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        colors,
-        extend: {
-            colors: {
-                primary: colors.rose,
-                'text-primary': colors.gray["800"],
-                'text-primary-dark': colors.gray["50"],
-                'rakuten': '#BF0000'
-            },
-            fontFamily: {
-                sans: [
-                    '游ゴシック体',
-                    'YuGothic',
-                    '游ゴシック',
-                    'Yu Gothic',
-                    'sans-serif',
-                ],
-            },
-        },
+  mode: 'jit',
+  purge: ['./src/**/*.{ts,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    colors,
+    extend: {
+      colors: {
+        primary: colors.rose,
+        'text-primary': colors.gray['800'],
+        'text-primary-dark': colors.gray['50'],
+        rakuten: '#BF0000',
+      },
+      fontFamily: {
+        sans: [
+          '游ゴシック体',
+          'YuGothic',
+          '游ゴシック',
+          'Yu Gothic',
+          'sans-serif',
+        ],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/line-clamp')],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
 };
