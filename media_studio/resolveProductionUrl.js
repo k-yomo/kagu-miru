3d09e4aae56d0070393136aa5e0dafb2910be5e9
k-yomo <kanji.yy@gmail.com>
2021-11-03 15:48:25 +0900

    Update preview url

--- media_studio/resolveProductionUrl.js
+++ media_studio/resolveProductionUrl.js
@@ -1,4 +1,4 @@
 
 export default function resolveProductionUrl(document) {
-    return `https://kagu-miru.com/media/posts/preview/${document.slug.current}`
+    return `https://kagu-miru.com/media/posts/${document.slug.current}/preview`
 }
