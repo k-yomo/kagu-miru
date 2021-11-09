04117d7c7583cd6053df4fe3f7e67da4cf13108d
k-yomo <kanji.yy@gmail.com>
2021-11-09 20:37:15 +0900

    Tweak link styling

--- frontend/src/components/LinkWithThumbnail.tsx
+++ frontend/src/components/LinkWithThumbnail.tsx
@@ -28,7 +28,7 @@ export default function LinkWithThumbnail({
           </div>
           <div className="ml-2 sm:ml-3 w-[70%]">
             <div className="mb-2 text-xl font-bold underline">{title}</div>
-            <div className="hidden sm:block">{truncate(subTitle, 100)}</div>
+            <div className="hidden sm:block text-text-secondary dark:text-text-secondary-dark">{truncate(subTitle, 100)}</div>
           </div>
         </div>
       </a>
