3db1155e6fb2306be7432d67d2a897cd3271fda8
k-yomo <kanji.yy@gmail.com>
2021-11-16 19:20:08 +0900

    Tweak styling of ecommerce site select box

--- frontend/src/components/PlatformSelect.tsx
+++ frontend/src/components/PlatformSelect.tsx
@@ -24,19 +24,19 @@ export default memo(function PlatformSelect({
 
   return (
     <div>
-      <div className="space-y-3">
+      <div className="space-y-2">
         <div className="flex items-center">
           <input
             id="platformCheckBoxRakuten"
             type="checkbox"
             name={ItemSellingPlatform.Rakuten}
             checked={platforms.includes(ItemSellingPlatform.Rakuten)}
-            className="h-5 w-5 rounded cursor-pointer"
+            className="h-4 w-4 rounded cursor-pointer"
             onChange={onChange}
           />
           <label
             htmlFor="platformCheckBoxRakuten"
-            className="ml-2 cursor-pointer"
+            className="ml-2 cursor-pointer text-sm"
           >
             {platFormText(ItemSellingPlatform.Rakuten)}
           </label>
@@ -47,12 +47,12 @@ export default memo(function PlatformSelect({
             type="checkbox"
             name={ItemSellingPlatform.YahooShopping}
             checked={platforms.includes(ItemSellingPlatform.YahooShopping)}
-            className="h-5 w-5 rounded cursor-pointer"
+            className="h-4 w-4 rounded cursor-pointer"
             onChange={onChange}
           />
           <label
             htmlFor="platformCheckBoxYahooShopping"
-            className="ml-2 cursor-pointer"
+            className="ml-2 cursor-pointer text-sm"
           >
             {platFormText(ItemSellingPlatform.YahooShopping)}
           </label>
