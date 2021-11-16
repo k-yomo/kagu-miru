35a420c77e95c44c2fd82414a346c5edfeb8bfd4
k-yomo <kanji.yy@gmail.com>
2021-11-16 21:53:48 +0900

    Refactor graphql request logging

--- backend/pkg/logging/graphqlinterceptor.go
+++ backend/pkg/logging/graphqlinterceptor.go
@@ -2,6 +2,8 @@ package logging
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql"
 	"go.uber.org/zap"
@@ -23,7 +25,26 @@ func (g GraphQLResponseInterceptor) Validate(schema graphql.ExecutableSchema) er
 }
 
 func (g GraphQLResponseInterceptor) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+	requestStarted := time.Now()
+	resp := next(ctx)
+
+	status := statusCode(len(resp.Errors) > 0)
 	oc := graphql.GetOperationContext(ctx)
-	Logger(ctx).Info(oc.OperationName, zap.String("query", oc.RawQuery), zap.Any("variables", oc.Variables))
-	return next(ctx)
+	Logger(ctx).Info(
+		fmt.Sprintf("%s - %s", oc.OperationName, status),
+		zap.String("type", "request"),
+		zap.String("operation", oc.OperationName),
+		zap.String("status", status),
+		zap.Int64("latencyMs", time.Since(requestStarted).Milliseconds()),
+		zap.String("query", oc.RawQuery),
+		zap.Any("variables", oc.Variables),
+	)
+	return resp
+}
+
+func statusCode(isErr bool) string {
+	if isErr {
+		return "error"
+	}
+	return "ok"
 }
