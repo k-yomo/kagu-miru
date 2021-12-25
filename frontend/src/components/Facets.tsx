8b00154c881e4aa014e744bcf1bd643afadbe0cf
k-yomo <kanji.yy@gmail.com>
2021-12-25 18:44:43 +0900

    Fix styling

--- frontend/src/components/Facets.tsx
+++ frontend/src/components/Facets.tsx
@@ -169,7 +169,7 @@ function FacetDropdown({
                   {facet.values.map((facetValue) => (
                     <div
                       key={facetValue.id}
-                      className="flex flex-between px-4 py-2 border-gray-100 dark:border-gray-800 text-sm"
+                      className="flex items-center flex-between px-4 py-2 border-gray-100 dark:border-gray-800 text-sm"
                       onClick={() =>
                         onClickFacet(facet.facetType, facetValue.id)
                       }
@@ -186,7 +186,7 @@ function FacetDropdown({
                         className="ml-2 cursor-pointer text-sm"
                       >
                         <div>{facetValue.name}</div>
-                        <div className="text-xs">
+                        <div className="text-xs text-text-secondary dark:text-text-secondary-dark">
                           {facetValue.count.toLocaleString()}件
                         </div>
                       </label>
