4c02ec12bc94294e209c65086195861924e1cc68
Kanji Yomoda <Kanji.yy@gmail.com>
2022-02-01 00:41:31 +0900

    Update TOC design and behavior when clicked (#251)

--- frontend/src/components/TableOfContents.tsx
+++ frontend/src/components/TableOfContents.tsx
@@ -1,30 +1,41 @@
-import React, { memo } from 'react';
-import Link from 'next/link';
-import { ChevronRightIcon } from '@heroicons/react/solid';
+import React, { memo, useEffect } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+// @ts-ignore
+import smoothScroll from 'smoothscroll-polyfill';
 
 interface Props {
   headings: string[];
 }
 
 export default memo(function TableOfContents({ headings }: Props) {
+  useEffect(() => {
+    smoothScroll.polyfill();
+  }, []);
+
   const onClickHeading = (i: number) => {
     const section = document.getElementsByTagName('h2');
     section[i]?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
   const listItems = headings.map((heading, i) => (
     <li key={heading} className="list-none ml-0">
-      <Link href={`#${i}`}>
-        <a className="flex items-center" onClick={() => onClickHeading(i)}>
-          <ChevronRightIcon className="h-5 w-5 min-w-5" />
-          <span className="ml-1">{heading}</span>
-        </a>
-      </Link>
+      <a
+        href={`#${i}`}
+        className="flex items-center mt-2 cursor-pointer"
+        onClick={() => onClickHeading(i)}
+      >
+        <span className="ml-1">{heading}</span>
+      </a>
     </li>
   ));
   return (
     <div className="px-2 sm:px-4 py-4 bg-gray-50 dark:bg-gray-900">
-      <div className="font-bold">目次</div>
-      <ul className="space-y-3 font-bold">{listItems}</ul>
+      <div className="flex items-center space-x-1 font-bold">
+        <ChevronDownIcon className="inline w-3 h-3" />
+        <div>目次</div>
+      </div>
+      <ul className="divide-y-[1px] divide-gray-200 dark:divide-gray-800">
+        {listItems}
+      </ul>
     </div>
   );
 });
